1488a41b0895e9936da7576dbf894183bb8278e7
gb <gico.gmail.com>
2019-01-29 01:26:01 +0100

    fix example

--- examples/sgpc3_first_running/sgpc3_first_running.ino
+++ examples/sgpc3_first_running/sgpc3_first_running.ino
@@ -1,20 +1,27 @@
 #include <sgpc3.h>
 
-//  Create an instance of SGPC3
+//Create an instance of SGPC3
 #ifdef ARDUINO_ARCH_ESP32
   SGPC3 SGCP3Sensor(22,21,200000);
 #else
-  SGPC3 SGCP3Sensor();
+  SGPC3 SGCP3Sensor;
 #endif
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   delay(1000);
-  // First step is to initialize the sensors; this should only fail if
-  // the board is defect, or the connection isn't working. Since there's nothing
-  // we can do if this fails, the code will loop forever if an error is detected
+  // Set Ultra Low power mode, sampling every 30s
   SGCP3Sensor.ultraLowPower();
+  // Init Sensor, is supposed that is was off for more than 1 week, correct initialization should set the correct baseline value and call the right initialization
+  /*
+    SGCP3Sensor.setBaselineValue(last_baseline);
+    SGCP3Sensor.initSGPC3(LT_5_MIN);
+    SGCP3Sensor.initSGPC3(LT_1_HOUR);
+    SGCP3Sensor.initSGPC3(LT_24_HOUR);
+    SGCP3Sensor.initSGPC3(LT_1_WEEK);
+   */
+  
   if (SGCP3Sensor.initSGPC3(LT_FOREVER) != 0) {
     Serial.print("Error while initializing sensors: ");
     Serial.print(SGCP3Sensor.getError());
@@ -23,10 +30,7 @@ void setup() {
       delay(1000);
     }
   }
-  // The SGP sensor has product type information and feature set stored
-  // the following code reads it out, and prints it to the serial console.
-  // This is purely to demo the function calls, and is not necessary to operate
-  // the sensor
+  // Read the spec of the sensor, type and version
   int type = SGCP3Sensor.getProductType();
   int p_version = SGCP3Sensor.getVersion();
   Serial.print("\n");
@@ -38,8 +42,7 @@ void setup() {
 
 void loop() {
   uint16_t tvoc = -1;
-
-  // we'll start by triggering a measurement of the VOC/CO2 sensor;
+  // we'll start by triggering a measurement of the VOC sensor;
   // it's important to do this first to make sure sleep timing is
   // correct. If the command succeeds, the local variables will
   // be set to the values we just read; if it fails, they'll be -1
@@ -50,6 +53,7 @@ void loop() {
   } else {
     tvoc = SGCP3Sensor.getTVOC();
   }
+  // get the baseline value that shuold be stored in non volatile memory
   if (SGCP3Sensor.getBaseline() != 0) {
     Serial.print("Error while getting Baseline: ");
     Serial.print(SGCP3Sensor.getError());
